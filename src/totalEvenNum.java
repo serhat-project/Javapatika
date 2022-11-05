bae6d6cdf7f3a28bb957be7ce730cbdba52a245a
serhat-project <sucar661@gmail.com>
2022-11-05 23:32:11 +0300

    "new changes added"

--- src/totalEvenNum.java
+++ src/totalEvenNum.java
@@ -4,7 +4,9 @@ public class totalEvenNum {
     public static void main(String[] args) {
 //        Java döngüler ile negatif bir değer girilene kadar kullanıcıdan girişleri kabul eden ve
 //        girilen değerlerden tek sayıları toplayıp ekrana basan programı yazıyoruz.
+//
 //        Scanner scan = new Scanner(System.in);
+
 //        int num;
 //        int total =0;
 //        do {
@@ -18,21 +20,25 @@ public class totalEvenNum {
 
         Scanner input = new Scanner(System.in);
         int sum = 0;
-        int num=0;
+        int num = 0;
 
-        for (int i=0; i >= 0; i++)
-        {
+        for (int i = 0; i >= 0; i++) {
             System.out.println("Please enter a positive number. Or enter a negative number to exit the program: ");
-            num=input.nextInt();
+            num = input.nextInt();
 
-            if (num<0)
+            if (num < 0) {
+                System.out.println("Negatif sayı girdiniz.");
                 break;
-            if (num%2==1) {
+            }
+
+            if (num % 2 == 1) {
                 sum += num;
             }
         }
         System.out.println("Sum of the even numbers you entered = " + sum);
 
-        }
     }
+}
 
+//        Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden
+//        4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz.
