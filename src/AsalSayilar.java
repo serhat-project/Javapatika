7f01bfa5ec32dda73da70b405da0a095d52936a9
serhat-project <sucar661@gmail.com>
2022-12-09 00:58:45 +0300

    new projact added

---
+++ src/AsalSayilar.java
@@ -0,0 +1,27 @@
+public class AsalSayilar {
+
+    public static void main(String[] args) {
+
+        // 1 ile 100 arasındaki asal sayıları kontrol etmek için döngü oluşturuyoruz
+        for(int i = 2; i <= 100; i++) {
+
+            // i'nin asal olup olmadığını kontrol eden değişkeni tanımladık
+            boolean asalMi = true;
+
+            // i'ye kadarki tüm sayıları kontrol ediyoruz
+            for(int j = 2; j < i; j++) {
+
+                // i'nin j'ye bölünebiliyorsa asal değil
+                if(i % j == 0) {
+                    asalMi = false;
+                    break;
+                }
+            }
+
+            // Eğer asal ise ekrana yazdırıyoruz
+            if(asalMi) {
+                System.out.println(i);
+            }
+        }
+    }
+}
