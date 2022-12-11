9a521c8dccfed5d1320999a6a112d06fc9d6100f
serhat-project <sucar661@gmail.com>
2022-12-11 23:47:56 +0300

    new projact added

---
+++ src/FighterBoxProject/FighterTest.java
@@ -0,0 +1,13 @@
+package FighterBoxProject;
+
+import FighterBoxProject.Fighter;
+
+public class FighterTest {
+    public static void main(String[] args) {
+        Fighter marc = new Fighter("Marc" , 15 , 100, 90, 0);
+        Fighter alex = new Fighter("Alex" , 10 , 95, 100, 0);
+        Ring r = new Ring(marc,alex , 90 , 100);
+        r.run();
+    }
+}
+
